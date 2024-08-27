18f4ab1ac144c48eb9c7e6fcfdfc6bc1f903713c
Brent-the-carpenter <beh337@gmail.com>
2024-08-26 23:28:20 -0400

    Implemented pygame.Clock object to limit screen refresh to 60 fps.

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import pygame
 from constants import *    
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    start = pygame.init()
-    running = True  
+
+    pygame.init()
+    clock= pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT  ))
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black" , rect=None)
         pygame.display.flip()
+        dt = clock.tick(60) /1000
+             
 if __name__ == "__main__":
+
     main()
